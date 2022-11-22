00e42e0afa087947d974cc1ebd3c043363c4953d
yunheekim2513(김윤희) <yunheekim2513@tmon.co.kr>
2022-11-22 23:27:51 +0900

    feat: ui 마크업 일부 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Home from './components/Home';
+import Work from './components/Work';
 
 export default function App() {
-  return <Nav />;
+  return (
+    <>
+      <Nav />
+      <Home />
+      <Work />
+    </>
+  );
 }
